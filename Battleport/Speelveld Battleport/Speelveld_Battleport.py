aed5e22f431d35f4145ab47a3e03b96335d3256b
sibrenvanek <0924655@hr.nl>
2017-01-20 11:01:40 +0100

    speelveld zonder menu

--- Battleport/Speelveld Battleport/Speelveld_Battleport.py
+++ Battleport/Speelveld Battleport/Speelveld_Battleport.py
@@ -5,11 +5,14 @@ menu_background = pygame.image.load('menu_background.bmp')
 #boot2 = pygame.image.load('boot2.bmp')
 #boot3 = pygame.image.load('boot3.bmp')
 #boot4 = pygame.image.load('boot4.bmp')
+pygame.font.get_fonts()
+pygame.font.init()
 h = 767
 w = round((h/9) * 16)
 background_blue = (25,25,112)
 white = (255,255,255)
 black = (0,0,0)
+grey = (200,200,200)
 screen = pygame.display.set_mode((w, h))
 def block(x, y, w, h, color):
     pygame.draw.rect(screen, color, [x, y, w, h])
@@ -18,32 +21,35 @@ def process_events():
         if event.type == pygame.QUIT:
             return True
     return False
-class Menu:
-    def buttons(self):
-        self.startbutton = block(1000, 150, 200, 70, white)
-        self.loadbutton = block(1000, 250, 200, 70, white)
-        self.highscoresbutton = block(1000, 350, 200, 70, white)
-        self.optionsbutton = block(1000, 450, 200, 70, white)
-        self.helpbutton = block(1000, 550, 200, 70, white)
-        self.exitbutton = block(1000, 650, 200, 70, white)
-    def draw(self):
-        while not(process_events()):
-            screen.fill((background_blue))
-            screen.blit(menu_background, (0,0))
-            self.buttons()
-            pygame.display.flip()
+def button(msg, x, y, w, h, color_active, color_inactive, action=None):
+     mouse = pygame.mouse.get_pos()
+     click = pygame.mouse.get_pressed()
+     if (x + w > mouse[0] > x) and (y + h > mouse[1] > y):
+         block(x, y, w, h, color_active)
+         if click[0] == 1 and action != None:
+             if action == "start":
+                 running = Battleport()
+     else:
+         block(x, y, w, h, color_inactive)
+     smallText = pygame.font.Font("freesansbold.ttf", 30)
+     textSurf, textRect = text_object(msg, smallText)
+     textRect.center = ((x + (w/2)), (y + (h/2)))
+     screen.blit(textSurf, textRect)
+def text_object(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 class Battleport:
     def __init__(self):
         screen.fill(background_blue)
     def buttons(self):
-        self.button1 = block(1100, 26, 80, 80, (white))
-        self.button2 = block(1190, 26, 80, 80, (white))
-        self.button3 = block(1280, 26, 80, 80, (white))
+        button("?", 1100, 26, 80, 80, grey, white)
+        button("||", 1190, 26, 80, 80, grey, white)
+        button("X", 1280, 26, 80, 80, grey, white)
     def draw(self):
-        while not(process_events()):
-            screen.fill((background_blue))
-            screen.blit(game_background, (0,0))
-            self.buttons()
-            pygame.display.flip()
+        screen.fill((background_blue))
+        screen.blit(game_background, (0,0))
+        self.buttons()
+        pygame.display.flip()
 running = Battleport()
-running.draw()
+while not(process_events()):
+    running.draw()
