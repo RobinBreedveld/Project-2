ee8232ba7bd13db3b38f5a08713d833619647bd0
sibrenvanek <0924655@hr.nl>
2017-01-23 10:18:53 +0100

    help menu, text added

--- Battleport/Battleport speelveld/Battleport_speelveld.py
+++ Battleport/Battleport speelveld/Battleport_speelveld.py
@@ -4,6 +4,7 @@ game_background = pygame.image.load('game_background.bmp')
 menu_background = pygame.image.load('menu_background.bmp')
 options_background = pygame.image.load('options_background.bmp')
 highscores_background = pygame.image.load('highscores_background.bmp')
+help_background = pygame.image.load('help_background.bmp')
 #boot2 = pygame.image.load('boot2.bmp')
 #boot3 = pygame.image.load('boot3.bmp')
 #boot4 = pygame.image.load('boot4.bmp')
@@ -34,12 +35,47 @@ def button(msg, x, y, w, h, color_active, color_inactive, action=None):
      else:
          block(x, y, w, h, color_inactive)
      smallText = pygame.font.Font("freesansbold.ttf", 30)
-     textSurf, textRect = text_object(msg, smallText)
+     textSurf, textRect = text_object(msg, smallText, black)
      textRect.center = ((x + (w/2)), (y + (h/2)))
      screen.blit(textSurf, textRect)
-def text_object(text, font):
-    textSurface = font.render(text, True, black)
+def text_object(text, font, color):
+    textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
+def text(msg, font, x, y, w, h, color):
+     font = font
+     textSurf, textRect = text_object(msg, font, color)
+     textRect.center = ((x + (w/2)), (y + (h/2)))
+     screen.blit(textSurf, textRect)
+class Help1:
+    def __init__(self):
+        self.type = "help1"
+        self.returnbutton = None
+    def buttons(self):
+        # returnbutton
+        self.returnbutton = button("Back to main menu", 50, 650, 400, 50, grey, white, "return")
+    def texts(self):
+        text("HELP", pygame.font.Font("freesansbold.ttf", 50), 50, 50, 300, 75, white)
+    def draw(self):
+        screen.fill((background_blue))
+        screen.blit(help_background, (0,0))
+        self.buttons()
+        self.texts()
+        pygame.display.flip()
+class Help2:
+    def __init__(self):
+        self.type = "help2"
+        self.returnbutton = None
+    def buttons(self):
+        # returnbutton
+        self.returnbutton = button("Back to game", 50, 650, 400, 50, grey, white, "return")
+    def texts(self):
+        text("HELP", pygame.font.Font("freesansbold.ttf", 50), 50, 50, 300, 75, white)
+    def draw(self):
+        screen.fill((background_blue))
+        screen.blit(help_background, (0,0))
+        self.buttons()
+        self.texts()
+        pygame.display.flip()
 class Highscores:
     def __init__(self):
         self.type = "highscores"
@@ -47,10 +83,13 @@ class Highscores:
     def buttons(self):
         #quitbutton
         self.quitbutton = button("Back to main menu", 50, 650, 400, 50, grey, white, "quit")
+    def texts(self):
+        text("Highscores", pygame.font.Font("freesansbold.ttf", 50), 50, 50, 300, 75, white)
     def draw(self):
         screen.fill((background_blue))
         screen.blit(highscores_background, (0,0))
         self.buttons()
+        self.texts()
         pygame.display.flip()
 class Options:
     def __init__(self):
@@ -58,11 +97,14 @@ class Options:
         self.quitbutton = None
     def buttons(self):
         # quitbutton
-        self.quitbutton = button("Bback to main menu", 50, 650, 400, 50, grey, white, "quit")
+        self.quitbutton = button("Back to main menu", 50, 650, 400, 50, grey, white, "quit")
+    def texts(self):
+        text("Options", pygame.font.Font("freesansbold.ttf", 50), 50, 50, 300, 75, white)
     def draw(self):
         screen.fill((background_blue))
         screen.blit(options_background, (0,0))
         self.buttons()
+        self.texts()
         pygame.display.flip()
 class Menu:
     def __init__(self):
@@ -71,41 +113,48 @@ class Menu:
         self.exitbutton = None
         self.optionsbutton = None
         self.highscoresbutton = None
+        self.helpbutton = None
     def buttons(self):
         # startbutton
         self.startbutton = button("Start The Game!", 950, 150, 300, 70, grey, white, "start")
         # loadbutton
-        loadbutton = button("Load A Game!", 950, 250, 300, 70, grey, white)
+        loadbutton = button("Load A Game!", 950, 250, 300, 70, grey, white, "load")
         # highscoresbutton
         self.highscoresbutton = button("Highscores", 950, 350, 300, 70, grey, white, "highscores")
         # optionsbutton
         self.optionsbutton = button("Options", 950, 450, 300, 70, grey, white, "options")
         # helpbutton
-        helpbutton = button("!!!HELP!!!", 950, 550, 300, 70, grey, white)
+        self.helpbutton = button("!!!HELP!!!", 950, 550, 300, 70, grey, white, "help")
         # exitbutton
         self.exitbutton = button("Exit The Game", 950, 650, 300, 70, grey, white, "exit")
-        
+    def texts(self):
+        text("Main Menu", pygame.font.Font("freesansbold.ttf", 50), 50, 50, 300, 75, white)
     def draw(self):
         screen.fill((background_blue))
         screen.blit(menu_background, (0,0))
         self.buttons()
+        self.texts()
         pygame.display.flip()
 class Battleport:
     def __init__(self):
         screen.fill(background_blue)
         self.type = "battleport"
         self.quitbutton = None
+        self.helpbutton = None
     def buttons(self):
         # helpbutton ingame
-        self.helpbutton = button("?", 1100, 26, 80, 80, grey, white)
+        self.helpbutton = button("?", 1100, 26, 80, 80, grey, white, "help")
         # pausebutton
         self.pausebutton = button("||", 1190, 26, 80, 80, grey, white)
         # quitbutton
         self.quitbutton = button("X", 1280, 26, 80, 80, grey, white, "quit")
+    #def texts(self):
+        
     def draw(self):
         screen.fill((background_blue))
         screen.blit(game_background, (0,0))
         self.buttons()
+        #self.texts()
         pygame.display.flip()
 running = Menu()
 while not(process_events()):
@@ -119,13 +168,23 @@ while not(process_events()):
             running = Options()
         elif running.highscoresbutton == "highscores":
             running = Highscores()
+        elif running.helpbutton == "help":
+            running = Help1()
     elif running.type == "battleport":
         if running.quitbutton == "quit":
             running = Menu()
+        elif running.helpbutton == "help":
+            running = Help2()
     elif running.type == "options":
         if running.quitbutton == "quit":
             running = Menu()
     elif running.type == "highscores":
         if running.quitbutton == "quit":
             running = Menu()
+    elif running.type == "help1":
+        if running.returnbutton == "return":
+            running = Menu()
+    elif running.type == "help2":
+        if running.returnbutton == "return":
+            running = Battleport()
     running.draw()
