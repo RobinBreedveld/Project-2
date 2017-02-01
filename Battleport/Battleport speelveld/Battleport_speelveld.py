f97f0ad6bb10cc9defbd625bc1dece2dc50f6036
xWasabiH <xindrah_95@hotmail.com>
2017-02-01 21:45:13 +0100

    defend > settings > battleport fix

--- Battleport/Battleport speelveld/Battleport_speelveld.py
+++ Battleport/Battleport speelveld/Battleport_speelveld.py
@@ -3619,7 +3619,15 @@ while not(process_events()):
 
     elif running.type == "options2":
         if running.quitbutton == "quit":
-            pygame.mixer.Sound.play(click)  
+            pygame.mixer.Sound.play(click)
+            ship1rood = running.ship1rood
+            ship2rood = running.ship2rood
+            ship3rood = running.ship3rood
+            ship4rood = running.ship4rood
+            ship1groen = running.ship1groen
+            ship2groen = running.ship2groen
+            ship3groen = running.ship3groen
+            ship4groen = running.ship4groen  
             turn = running.turn
             running = Battleport(turn,True)
 
